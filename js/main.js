25c3c128ae602ae788f975cbd453b63fc83af3d0
Jeremy Ward <jeremy2929@twc.com>
2017-02-01 19:01:31 -0600

    Login and Transactions page functional

--- js/main.js
+++ js/main.js
@@ -5,237 +5,197 @@
 import React from 'react'
 import Validator from 'validator'
 import { ajax } from 'jquery'
-
-
-//var firebase = require("firebase/app");
-//require("firebase/auth");
-//require("firebase/database");
-
-
-/*
-var userId = firebase.auth().currentUser.uid;
-return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-  var username = snapshot.val().username;
-  // ...
-});
-*/
-var userId = "jeremy2929"
-//var userId = firebase.auth().currentUser.uid;
-var database = firebase.database();
-
-
-//var playersRef = firebase.database().ref("players/");
-
-var ref = firebase.database().ref(userId+"/"+"transactions");
-ref.on("value", function(allData) {
-   //console.log(allData.val());
-   var test = allData.val()
-   console.log(test[0]);
-}, function (error) {
-   console.log("Error: " + error.code);
-});
-
-
-
+var user = "none"
 export default React.createClass({
   componentDidMount(){
 
-  //  final FirebaseDatabase database = FirebaseDatabase.getInstance()
+  },
+  getInitialState(){
+    return{
+      textValue: "",
+      allData: ["text messages"],
+      data: [""]
+    }
+    this.setState({user})
 
-/*
-      ajax({
-      url: "https://tiny-tiny.herokuapp.com/collections/jeremy2929-test",
-      dataType: "json",
-      success: this.onInitialAjaxLoadSuccess,
-      error: this.onAjaxLoadError
-    })
-*/
-
-},
-writeNewPost(userId) {
-  // A post entry.
-
-/*
-  var newData
-  this.state.newData = []
-  this.state.newData[0] = this.state.data[245]
-  this.state.newData[1] = this.state.data[246]
-  this.state.newData[2] = this.state.data[246]
-  this.state.newData[3] = this.state.data[247]
-  this.state.newData[4] = this.state.data[248]
-  this.state.newData[5] = this.state.data[249]
-  this.state.newData[6] = this.state.data[250]
-  this.state.newData[7] = this.state.data[251]
-
-  this.state.data = this.state.newData
-*/
+  },
 
 
-/*
-    var postData = {
-    author: "clown",
-    body: "body",
-    title: "title",
-    starCount: 0,
-    authorPic: "picture"
-  };
-  // Get a key for a new Post.
-
-*/
-  var newPostKey = firebase.database().ref().child('posts').push().key;
-
-
-
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  var tempUser = firebase.auth().currentUser.email.split("@")
-  var currentUser = tempUser[0]
-  //updates['/posts/' + newPostKey] = this.state.data;
-  updates[currentUser + '/' + "transactions"] = this.state.data;
-  //updates['/posts/' + newPostKey] = postData;
-  //updates['/user-posts/' + "3333" + '/' + newPostKey] = postData;
-
-  return firebase.database().ref().update(updates);
-},
-// [END write_fan_out]
-
-
-//****************
-
-//  getInitialState(){
-//     return
-
-
-
-    //
-    // {
-    //   textValue: "",
-    //   textMsgs: ["text messages"],
-    //   data: [""]
-    // }
-
-
-//  },
-
-/*
   onInitialAjaxLoadSuccess(response){
     var holdResponse = response.reverse()
     this.setState({
-      textMsgs: holdResponse,
+      database: holdResponse,
+      allData: holdResponse,
       data: holdResponse
     })
+    var databaseLength = this.state.database.length
+    var j = 0
+    this.state.data = []
+    for (var i = 0; i < databaseLength; i++){
 
+      if (this.state.database[i].user != undefined && this.state.database[i].password != undefined){
 
+              if (this.state.database[i].user === this.state.user
+                  && this.state.database[i].password === this.state.pswd)
+                {
+                  this.state.data[j] = this.state.database[i]
+                  j++
+                }
+        }
+    }
+    this.state.allData = this.state.data
+    this.setState(this.state.data)
+    console.log("Alldata=",this.state.allData);
+    console.log("data=",this.state.data);
   },
   onPostAjaxLoadSuccess(response){
     this.setState({
-      textMsgs: this.state.textMsgs.concat(response),
+      database: this.state.allData.concat(response),
+      allData: this.state.allData.concat(response),
       data: this.state.data.concat(response)
     })
   },
   onAjaxLoadError(response){
     alert("Failure to connect to URL")
   },
-
-  */
-
-
+  onSubmitUser(e){
+    var user = this.refs.userInput.value
+    var pswd = this.refs.passwordInput.value
+    this.refs.userInput.value = ""
+    this.refs.passwordInput.value = ""
+    this.setState({user})
+    this.setState({pswd})
+    //************************************
+    // mount ajax
+    //************************************
+    ajax({
+    url: "https://tiny-tiny.herokuapp.com/collections/jeremy2929-test1",
+    dataType: "json",
+    success: this.onInitialAjaxLoadSuccess,
+    error: this.onAjaxLoadError
+    })
+  },
+  onSignOut(){
+    var user = "none"
+    var pswd = "none"
+    this.setState({user})
+  },
   onClickSubmit(e){
-    e.preventDefault()
     var currentDate = Date().substring(4,16)
-    var textInputValue = this.refs.textInput.value
+    e.preventDefault()
+    var textInputValue = this.refs.descriptionInput.value
     var amountInputValue = this.refs.amountInput.value
-    var currentUser = firebase.auth().currentUser.email
-
+    var userID = this.state.user
+    var passwordID = this.state.pswd
     if (textInputValue != ""){
-
-/*
       ajax({
-        url: "https://tiny-tiny.herokuapp.com/collections/jeremy2929-test",
+        url: "https://tiny-tiny.herokuapp.com/collections/jeremy2929-test1",
         dataType: "json",
         type: "POST",
         data:
             {
-              userId: currentUser,
-              amount: amountInputValue,
-                text: textInputValue,
-                date: currentDate
+              user: userID,
+              password: passwordID,
+              date: currentDate,
+              text: textInputValue,
+            amount: amountInputValue,
             },
         success: this.onPostAjaxLoadSuccess,
         error: this.AjaxLoadError
       })
-
-  */
-      var showing = this.refs.Show8.className
+      var showing = this.refs.Show15.className
       if (showing==="hiddenButton"){
-        var dataLength = this.state.data.length-8
-        this.state.data=[]
-        for (var j = dataLength;j<dataLength+8; j++){
-          this.state.data[j]=this.state.textMsgs[j]
+        var dataLength = this.state.data.length-5
+        if (dataLength>5){
+            this.state.data=[]
+            for (var j = dataLength;j<dataLength+5; j++){
+              this.state.data[j]=this.state.allData[j]
+            }
         }
       }
-      this.refs.textInput.value = ""
+      this.refs.descriptionInput.value = ""
       this.refs.amountInput.value = ""
     }
     this.setState(this.state.data)
-    this.setState(this.state.textMsgs)
+    this.setState(this.state.allData)
   },
   onClickShowAll(){
-    this.refs.Show8.className="recentView"
+
+    this.refs.Show15.className="visibleButton"
     this.refs.ShowAll.className="hiddenButton"
-    this.state.data = this.state.textMsgs
+    this.state.data = this.state.allData
     this.setState(this.state.data)
   },
-  onSignOut(){
+  onClickShow15(){
+    console.log("before=",this.state.data);
+    console.log("before=",this.state.allData);
+    this.refs.ShowAll.className="visibleButton"
+    this.refs.Show15.className="hiddenButton"
+    var dataLength = this.state.allData.length
+console.log("len=",dataLength);
+    if (dataLength>4){
+        this.state.data=[]
+        for (var j = 0;j<5; j++){
+          console.log(j);
+          this.state.data[j]=this.state.allData[j]
+          this.setState(this.state.data)
+        }
 
-    console.log("sign out");
-    document.getElementById('signin_main').className="mdl-layout__header mdl-color-text--whitemdl-color--light-blue-700"
-    document.getElementById('signin_header').className="hidden"
-    document.getElementById('detailsPage').className="pageSection"
-    location.window.reload(true)
-  //  document.getElementById('app').class="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700"
-
-  },
-  onClickShow8(){
-    this.writeNewPost(userId)
-    this.refs.ShowAll.className="recentView"
-    this.refs.Show8.className="hiddenButton"
-    this.state.data=this.state.textMsgs
-    var dataLength = this.state.data.length-8
-    this.state.data=[]
-    for (var j = dataLength;j<dataLength+8; j++){
-      this.state.data[j]=this.state.textMsgs[j]
     }
+    console.log("after=",this.state.data);
+    console.log("after=",this.state.allData);
     this.setState(this.state.data)
-    // <button className="visibleSignOut" id="quickstart-sign-in" name="signin" >Sign Out</button>
 
   },
-
   render() {
+  console.log("render=",this.state.allData);
+    if (this.state.user === undefined || this.state.user === "none"){
     return(
-    <main id="details_main">
-      <section id="detailsPage" className="hidden">
-          <h1 className="liteTitle">DollarTrack</h1>
+    <main>
+      <section>
+          <article className="loginTitleSection">
+            <img className="loginImage" src="styles/titleLogin.jpg"></img>
+            <h1 className="titleLogin">DollarTrak</h1>
+            <img className="loginImage" src="styles/titleLogin.jpg"></img>
+          </article>
+          <p>Login</p>
+          <input className="userNameInput" ref="userInput"></input>
+          <input className="passwordInput" ref="passwordInput"></input>
+          <button className="submitUser" onClick={this.onSubmitUser}></button>
+      </section>
+    </main>
+  )
+} else if (this.state.user != undefined || this.state.user != "none"){
+     return (
+      <main>
+        <section className="pageSection">
+          <h1 className="chatTitle">Daily Transactions</h1>
             <ul id="list" className="newList">
               {
                 this.state.data.map((record, i)=>{
-                  if (record.amount != undefined && record.text != ""){
-                    return <article className="itemBox">
-                              <p key="1" className="transDate">{record.date}</p>
-                              <p key="3" className="amountBox">{record.amount}</p>
-                              <p key="2" className="descriptionBox" key={i}>{record.text}</p>
+                  if (record.date != undefined
+                   && record.text != "")
+                   {
+                    return <article className="eachRecordContainer" key={i}>
+                              <p className="transDate">{record.date}</p>
+                              <p className="transAmount">{record.amount}</p>
+                              <p className="transDesc">{record.text}</p>
+                              <p className="transID">{record.user}</p>
                             </article>
                   }
               })
             }
             </ul>
             <input className="amountItem" placeholder="amount" ref="amountInput" type="text"/>
-            <input className="descriptionItem" placeholder="item description" ref="textInput" type="text"/>
-            <input className="createChat" type="submit" onClick={this.onClickSubmit}/>
-            <button className="hiddenButton" ref="Show8" onClick={this.onClickShow8}>Show Last 8 Transactions Only</button>
-            <button className="recentView" ref="ShowAll" onClick={this.onClickShowAll}>    Show All Transactions    </button>
+            <input className="descriptionItem" placeholder="enter description" ref="descriptionInput" type="text"/>
+            <button className="createChat" type="submit" onClick={this.onClickSubmit}>Submit</button>
+            <button className="visibleButton" ref="Show15" onClick={this.onClickShow15}>Show Last 5 Transactions Only</button>
+            <button className="hiddenButton" ref="ShowAll" onClick={this.onClickShowAll}>    Show All Transactions    </button>
+            <button className="signOut" onClick={this.onSignOut}>Sign out</button>
       </section>
     </main>
-    )
+  )
+  }
+
   }
 })
