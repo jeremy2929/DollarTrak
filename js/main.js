f948c9399c946e040767fef7883908e1e7da87c8
Jeremy Ward <jeremy2929@twc.com>
2017-02-01 22:28:25 -0600

    Styling added for Login and Transactions page

--- js/main.js
+++ js/main.js
@@ -1,52 +1,55 @@
-/*   This script creates a chat room where user can read stored chats and user has ability
-    to add chat messages. All chats are stored along with date on external server.
-    The user has the option to click button to show only last 15 chat messages or click button to
-    show all messages  */
 import React from 'react'
-import Validator from 'validator'
 import { ajax } from 'jquery'
 var user = "none"
 export default React.createClass({
   componentDidMount(){
-
+    //@TODO: remove this if not used
   },
   getInitialState(){
     return{
       textValue: "",
-      allData: ["text messages"],
-      data: [""]
+        allData: ["text messages"],
+           data: [""]
     }
     this.setState({user})
-
   },
-
-
   onInitialAjaxLoadSuccess(response){
     var holdResponse = response.reverse()
     this.setState({
       database: holdResponse,
-      allData: holdResponse,
-      data: holdResponse
+       allData: holdResponse,
+          data: holdResponse
     })
     var databaseLength = this.state.database.length
     var j = 0
     this.state.data = []
     for (var i = 0; i < databaseLength; i++){
 
-      if (this.state.database[i].user != undefined && this.state.database[i].password != undefined){
+      if (this.state.database[i].user != undefined && this.state.database[i].password != undefined)
+        {
 
-              if (this.state.database[i].user === this.state.user
-                  && this.state.database[i].password === this.state.pswd)
-                {
-                  this.state.data[j] = this.state.database[i]
-                  j++
-                }
+          if (this.state.database[i].user === this.state.user
+              && this.state.database[i].password === this.state.pswd)
+            {
+              this.state.data[j] = this.state.database[i]
+              j++
+            }
         }
     }
+//*********************************************************************
+    //@TODO  if new user with no records, need to set at least one record with user ID
+    console.log("j=",j);
+    if (j === 0){this.state.data[0].user = "jj"}
+//*********************************************************************
     this.state.allData = this.state.data
+    var dataStart = this.state.allData.length-5
+    if (dataStart > 0){
+        this.state.data = []
+        for (var j = 0; j < 5; j++){
+          this.state.data[j]=this.state.allData[dataStart+j]
+        }
+    }
     this.setState(this.state.data)
-    console.log("Alldata=",this.state.allData);
-    console.log("data=",this.state.data);
   },
   onPostAjaxLoadSuccess(response){
     this.setState({
@@ -103,15 +106,12 @@ export default React.createClass({
         success: this.onPostAjaxLoadSuccess,
         error: this.AjaxLoadError
       })
-      var showing = this.refs.Show15.className
-      if (showing==="hiddenButton"){
-        var dataLength = this.state.data.length-5
-        if (dataLength>5){
-            this.state.data=[]
-            for (var j = dataLength;j<dataLength+5; j++){
-              this.state.data[j]=this.state.allData[j]
-            }
-        }
+      var dataStart = this.state.allData.length-5
+      if (dataStart > 0){
+          this.state.data = []
+          for (var j = 0; j < 5; j++){
+            this.state.data[j]=this.state.allData[dataStart+j]
+          }
       }
       this.refs.descriptionInput.value = ""
       this.refs.amountInput.value = ""
@@ -120,82 +120,78 @@ export default React.createClass({
     this.setState(this.state.allData)
   },
   onClickShowAll(){
-
-    this.refs.Show15.className="visibleButton"
-    this.refs.ShowAll.className="hiddenButton"
+    this.refs.Show5.className = "visibleButton"
+    this.refs.ShowAll.className = "hiddenButton"
     this.state.data = this.state.allData
     this.setState(this.state.data)
   },
-  onClickShow15(){
-    console.log("before=",this.state.data);
-    console.log("before=",this.state.allData);
-    this.refs.ShowAll.className="visibleButton"
-    this.refs.Show15.className="hiddenButton"
-    var dataLength = this.state.allData.length
-console.log("len=",dataLength);
-    if (dataLength>4){
-        this.state.data=[]
-        for (var j = 0;j<5; j++){
-          console.log(j);
-          this.state.data[j]=this.state.allData[j]
-          this.setState(this.state.data)
+  onClickShow5(){
+    this.refs.ShowAll.className = "visibleButton"
+    this.refs.Show5.className = "hiddenButton"
+    var dataStart = this.state.allData.length-5
+    if (dataStart > 0){
+        this.state.data = []
+        for (var j = 0; j < 5; j++){
+          this.state.data[j]=this.state.allData[dataStart+j]
         }
-
     }
-    console.log("after=",this.state.data);
-    console.log("after=",this.state.allData);
     this.setState(this.state.data)
-
   },
   render() {
-  console.log("render=",this.state.allData);
-    if (this.state.user === undefined || this.state.user === "none"){
-    return(
-    <main>
-      <section>
-          <article className="loginTitleSection">
-            <img className="loginImage" src="styles/titleLogin.jpg"></img>
-            <h1 className="titleLogin">DollarTrak</h1>
-            <img className="loginImage" src="styles/titleLogin.jpg"></img>
-          </article>
-          <p>Login</p>
-          <input className="userNameInput" ref="userInput"></input>
-          <input className="passwordInput" ref="passwordInput"></input>
-          <button className="submitUser" onClick={this.onSubmitUser}></button>
-      </section>
-    </main>
-  )
-} else if (this.state.user != undefined || this.state.user != "none"){
-     return (
-      <main>
-        <section className="pageSection">
-          <h1 className="chatTitle">Daily Transactions</h1>
-            <ul id="list" className="newList">
-              {
-                this.state.data.map((record, i)=>{
-                  if (record.date != undefined
-                   && record.text != "")
-                   {
-                    return <article className="eachRecordContainer" key={i}>
-                              <p className="transDate">{record.date}</p>
-                              <p className="transAmount">{record.amount}</p>
-                              <p className="transDesc">{record.text}</p>
-                              <p className="transID">{record.user}</p>
-                            </article>
-                  }
-              })
-            }
-            </ul>
-            <input className="amountItem" placeholder="amount" ref="amountInput" type="text"/>
-            <input className="descriptionItem" placeholder="enter description" ref="descriptionInput" type="text"/>
-            <button className="createChat" type="submit" onClick={this.onClickSubmit}>Submit</button>
-            <button className="visibleButton" ref="Show15" onClick={this.onClickShow15}>Show Last 5 Transactions Only</button>
-            <button className="hiddenButton" ref="ShowAll" onClick={this.onClickShowAll}>    Show All Transactions    </button>
-            <button className="signOut" onClick={this.onSignOut}>Sign out</button>
-      </section>
-    </main>
-  )
-  }
+  //  this.state.user = "jw"
+  // <h2 className="loginLabel">Login Screen:</h2>
 
-  }
+    if (this.state.user === undefined || this.state.user === "none")
+    {
+      return(
+        <main>
+          <section className="loginPage">
+              <article className="loginTitleSection">
+                <img className="loginImage" src="styles/titleLogin.jpg"></img>
+                <h1 className="titleLogin">DollarTrak</h1>
+                <img className="loginImage" src="styles/titleLogin.jpg"></img>
+              </article>
+              <article className="loginButtonsSection">
+                <h2 className="loginLabel">     </h2>
+                <input className="userNameInput" placeholder="  user name" ref="userInput"></input>
+                <input className="passwordInput" placeholder="  password" ref="passwordInput"></input>
+                <button className="submitUser" onClick={this.onSubmitUser}>LOGIN</button>
+              </article>
+          </section>
+        </main>
+      )
+    } else if (this.state.user != undefined || this.state.user != "none")
+      {
+        return (
+          <main>
+            <section className="pageSection">
+              <h1 className="transactionsTitle">DollarTrak - Daily Transactions</h1>
+              <h2 className="userName" ref="userName">User:  {this.state.data[0].user}</h2>
+              <ul id="list" className="newList">
+                {
+                  this.state.data.map((record, i)=>{
+                    if (record.date != undefined
+                     && record.text != "")
+                     {
+                      return <article className="eachRecordContainer" key={i}>
+                                <p className="transDate">{record.date}</p>
+                                <p className="transAmount">${record.amount}</p>
+                                <p className="transDesc">{record.text}</p>
+                                <p className="transID">{record.user}</p>
+                            </article>
+                    }
+                })
+              }
+              </ul>
+              <input className="amountItem" placeholder=" $ amount" ref="amountInput" type="text"/>
+              <input className="descriptionItem" placeholder="  description of purchase" ref="descriptionInput" type="text"/>
+              <button className="createChat" type="submit" onClick={this.onClickSubmit}>Submit</button>
+              <button className="hiddenButton" ref="Show5" onClick={this.onClickShow5}>Show Last 5 Transactions Only</button>
+              <button className="visibleButton" ref="ShowAll" onClick={this.onClickShowAll}>    Show All Transactions    </button>
+              <button className="signOut" onClick={this.onSignOut}>Log Out</button>
+            </section>
+          </main>
+        )
+      }
+    }
 })
